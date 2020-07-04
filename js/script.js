e191d56fff7ea514be380c3f6f987943709e5f4d
Ayush Jangir <jangirayush59@gmail.com>
2020-07-04 23:35:25 +0530

    Popup added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,29 @@
 $(document).ready(function () {
-    $('.card').addClass('visible1');
-    setTimeout(function () {
-        $('.card').addClass('visible2');
-    }, 600);
+
+    $(window).click(function () {
+        $('.popup').css('opacity', '0');
+        setTimeout(function () {
+            $('.popup').css('display', 'none');
+        }, 400)
+    });
+
+    $('.grid-card .card-title').click(function (e) {
+        e.stopPropagation();
+        $('.popup').css('display', 'initial');
+        setTimeout(function () {
+            $('.popup').css('opacity', 1);
+        }, 100);
+        setTimeout(function () {
+            $('.card').addClass('visible2');
+        }, 400);
+        setTimeout(function () {
+            $('.card1-content').css('opacity', '1');
+        }, 1000);
+    });
+
+    $('.card').click(function (e) {
+        e.stopPropagation();
+    });
 
     $('input[type=\'text\']').each(function () {
         if ($(this).val() !== '') $('#' + $(this).attr('id') + '_label').addClass('out');
