3e3100dbfd527fbf215d13fd1b59b122b79cb9ba
Ayush Jangir <52877162+099Ayush@users.noreply.github.com>
2020-07-04 16:20:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.card').addClass('visible1');
+    setTimeout(function () {
+        $('.card').addClass('visible2');
+    }, 600);
+
+    $('input[type=\'text\']').each(function () {
+        if ($(this).val() !== '') $('#' + $(this).attr('id') + '_label').addClass('out');
+        else $('#' + $(this).attr('id') + '_label').removeClass('out');
+        $(this).focus(function () {
+            $('#' + $(this).attr('id') + '_label').addClass('out');
+        });
+        $(this).blur(function () {
+            if ($(this).val() === '') {
+                $('#' + $(this).attr('id') + '_label').removeClass('out');
+            }
+        });
+    });
+
+});
