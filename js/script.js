ad3904efd7c2bea92042a70ee8dbbb376d467076
Ayush Jangir <jangirayush59@gmail.com>
2020-07-07 10:46:13 +0530

    Add comments form

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ $(document).ready(function () {
         e.stopPropagation();
     });
 
-    $('input[type=\'text\']').each(function () {
+    $('input[type=\'text\'], textarea').each(function () {
         if ($(this).val() !== '') $('#' + $(this).attr('id') + '_label').addClass('out');
         else $('#' + $(this).attr('id') + '_label').removeClass('out');
         $(this).focus(function () {
@@ -47,5 +47,33 @@ $(document).ready(function () {
 
     $('.nav-button').click(function () {
         $('nav ul, .nav-button').toggleClass('open');
+    });
+
+    const regex = /^([a-zA-Z0-9_.+-])+@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    const regex2 = /^[a-zA-Z\s]+$/;
+    const regex3 = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    $('#comments').submit(function (e) {
+        if ($('#name').val() === '' || $('#email').val() === 0 || $('#comment').val() === '') {
+            $('.text-danger').css('opacity', '1').css('visibility', 'visible');
+            $('.text-danger').html('Please fill all * marked fields.');
+            return false;
+        }
+        if (!regex2.test($('#name').val())) {
+            $('.text-danger').css('opacity', '1').css('visibility', 'visible');
+            $('.text-danger').html('Please enter a valid name.');
+            return false;
+        }
+        if (!regex.test($('#email').val())) {
+            $('.text-danger').css('opacity', '1').css('visibility', 'visible');
+            $('.text-danger').html('Please enter a valid e-mail address.');
+            return false;
+        }
+        if ($('#website').val() !== '' && !regex3.test($('#website').val())) {
+            $('.text-danger').css('opacity', '1').css('visibility', 'visible');
+            $('.text-danger').html('Please enter a valid website or leave it blank.');
+            return false;
+        }
+        $('.text-danger').css('opacity', '0');
+        return true;
     });
 });
